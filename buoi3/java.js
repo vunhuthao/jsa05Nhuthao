c6794a7e39edb77c4daaedb90163b5e5874a95da
vunhuthao <nhuthao18101995@gmail.com>
2023-08-20 12:03:56 +0700

    buoi3

---
+++ buoi3/java.js
@@ -0,0 +1,63 @@
+let myA = [4, 5, 7, 8, 9, 1];
+//console.log (myA);
+
+//for(let i=0; i < myA.length; i++){
+//    console.log("Giá trị của phần tử thứ: ", i, " là ", myA[i])
+//}
+
+//myA[2] = 6;
+//console.log(myA)
+
+//myA.splice(2, 1, 7, 8);
+//console.log(myA)
+
+//console.log(myA.length);   //độ dài mảng 
+
+//myA.push(7)  //thêm phần tử vào cuối
+//console.log(myA)
+
+//myA.pop()  //xoá phần tử ở cuối
+//console.log(myA)
+ 
+//myA.unshift(0)   //thêm một phần tử ở đầu
+//console.log(myA)
+
+//myA.shift()    // xoá phần tử đầu
+//console.log(myA)
+
+
+let dog = {
+    name: 'Alaska',
+    age: 4
+}
+//dog.name = 'chihuahua'
+
+//dog.action = 'walk'
+
+//delete( dog.action)
+//console.log(dog)
+
+//let cat = new Object({
+//    name:'Tom',
+//    age: 3,
+//    enemy:{
+//        name: 'Alaska',
+//        age: 5
+//    }
+//})
+//console.log(cat)
+
+//let dog1 = ["alaska", 4]
+//let dog2 = ["corgi", 5]
+//let dogs = [dog1, dog2]
+//console.log(dogs)
+
+//console.log (dogs[1][1])
+
+//let gioT = [
+//    ['xoai', 2],
+//    ['dua hau', 3]
+//]
+//for(let i=0; i<gioT.length; i++){
+//    console.log(gioT[i][0] + " : " + gioT[i][1])
+//}
---
+++ buoi3/main.js
@@ -0,0 +1,15 @@
+let arr=["toán", "ngữ văn", "TA"]
+//for (let i in arr){
+//    console.log(arr[i])
+//}
+
+//let obj = {
+//    'name': "nguyên văn đẩu",
+//    'sex': "k xác định",
+//    'crime': "vi pham hành chính"
+//}
+
+//for(let i in obj)
+//    console.log(obj[i])
+//for (let value of arr)
+//     console.log(value)
