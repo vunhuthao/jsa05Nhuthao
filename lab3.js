5badc192a7841b8b8bc579de857c207161381681
vunhuthao <nhuthao18101995@gmail.com>
2023-08-25 19:54:17 +0700

    lab3

---
+++ lab3.js
@@ -0,0 +1,39 @@
+//bài 1
+//let myAr = ["   1   ", "  111  "," 11111 ","1111111"]
+//for (i = 0; i < 4; i++){
+//    console.log(myAr[i]);
+//}
+
+//bài2
+//myArr = [9,7,9,0,7,8,287,123,456]
+//for (i = 0; i < 10; i++){
+//    if (myArr[i] % 2 == 0){
+//        console.log(myArr[i])
+//    }
+//}
+
+//bài3
+//let person = prompt("Mời bạn nhập tên của mình", "");
+//if (person == null || person == "") {
+//  alert("ờ vậy thôi");
+//} else {
+// let age = prompt("bạn bao nhiêu tuổi?","")
+//}
+//if (age == null || age == "") {
+//    alert("ờ,chào");
+//} else{
+//    let birth = prompt("bạn sinh ngày bao nhiêu","")
+//}
+//if (birth == null ||birth  == "") {
+//    alert("ờ,chào");
+//} else{
+//    let color = prompt("Màu yêu thích của bạn?","")
+//}
+//if (color == null ||color  == "") {
+//    alert("ờ,chào");
+//} else{
+//    alert("cảm ơn")
+//}
+
+
+
