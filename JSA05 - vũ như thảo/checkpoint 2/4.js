cc4255a409465502177f3c9c38f82d2f8c4a3b38
vunhuthao <nhuthao18101995@gmail.com>
2023-10-01 11:26:20 +0700

    checkpoint2

---
+++ JSA05 - vũ như thảo/checkpoint 2/4.js
@@ -0,0 +1,22 @@
+const fetchData = async () => {
+    try {
+        const response = await fetch('https://picsum.photos/v2/list');
+        const data = await response.json();
+        console.log(data);
+
+        for (let i of fetchData.data){
+
+    let imgContainer = document.createElement("div")
+    imgContainer.classList.add("image-container")
+
+    let image = document.createElement("img")
+    image.setAttribute("src", i.url)
+    imgContainer.appendChild(image)
+    //card.appendChild(imgContainer) 
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
+
+fetchData();
